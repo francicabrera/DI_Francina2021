8522aa052f73d72ff43b413ea255b899c17d7394
FrancinaCabrera <72362160+francicabrera@users.noreply.github.com>
2021-07-22 19:58:17 +0100

    code

---
+++ Slum_detection/Slum_detectionR.R
@@ -0,0 +1,42 @@
+#########################
+# Extracting slums from satellite imagery. R
+# Francina Cabrera, 2021
+# Description:
+
+# Code source: 
+# https://pages.cms.hu-berlin.de/EOL/gcg_eo/index.html
+# https://andrewmaclachlan.github.io/CASA0005repo/advanced-raster-analysis.html
+# http://www.qgistutorials.com/en/docs/3/raster_mosaicing_and_clipping.html
+
+################################################################################
+
+# Load all the required packages
+#install.packages('utils')
+
+library(sp)
+library(raster)
+library(rgdal)
+library(sf)
+library(here)
+library(rgeos)
+library(rasterVis)
+library(ggplot2)
+library(stringr)
+library(fs)
+library(tidyverse)
+library(maptools)
+library(tmap)
+library(GGally)
+library(hexbin)
+library(reshape2)
+library(randomForest)
+library(caTools)
+library(googledrive)
+library(utils)
+
+################################################################################
+
+# 1. Load and prepare the data
+# a.1) Distrito Nacional's boundary
+
+
